27a2028d8190f3dbfee63159271caea2fa3a7913
rubenavaldez <rubenavaldez@yahoo.com>
2021-08-31 20:21:49 -0700

    added functions from smart sparrow

--- app.js
+++ app.js
@@ -5,11 +5,31 @@
 //Precession 23.5 degrees * 2  per 26000 years
 
 //in seconds
-let SiderealDay = (23 * (60**2)) + (56*60)+ 4.0905
-console.log(SiderealDay + " seconds") 
+// let SiderealDay = (23 * (60**2)) + (56*60)+ 4.0905
+// console.log(SiderealDay + " seconds") 
 
-let precession = (23.5 * 2) / 26000 
-console.log(precession + '°'+ " per year") 
+// let precession = (23.5 * 2) / 26000 
+// console.log(precession + '°'+ " per year") 
 
-let SiderealYear = 365.25;
-console.log(SiderealYear + ' days')
+// let SiderealYear = 365.25;
+// console.log(SiderealYear + ' days')
+
+//2440587.5 days + UNIX TIME in days === Julian Day
+// for(let i =11; i <= 32;i++){
+// let x= i
+// let num = (3*(x**2) + (6*x))/ (2*x) 
+// console.log(`x =${i}  y=${num}`)
+
+// }
+
+// let x = .5
+// //console.log(-8*x+7)
+// let y =  10** (-8*x +7)
+// console.log(y)
+// let x = 5.0
+// let y = Math.log10(8+(x/10))
+// console.log(y)
+// let x = 5.0
+// let y = Math.log(8+(x/10))
+// console.log(y)
+

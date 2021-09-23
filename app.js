977d9caaf9e4fdb05fcff6a2b3d2860a05ce67f3
rubenavaldez <rubenavaldez@yahoo.com>
2021-09-22 21:43:39 -0700

    added luminosity calculations

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Sidereal day 23 hours 56 minutes 4.0905 seconds
 // Solar day 24 hours 
- //Sidereal year 365.25
+//Sidereal year 365.25
 //Calendar year 365 days + leap year 
 //Precession 23.5 degrees * 2  per 26000 years
 
@@ -33,3 +33,27 @@
 // let y = Math.log(8+(x/10))
 // console.log(y)
 
+// Lumonisty (9.94 x 10^6) * pi * flux/(parralax^2)
+
+
+function LumonistyCalculator(starName, fluxString, parralax) {
+    let flux = eval(fluxString)
+    //console.log(flux)
+    let C = 9.94 * (10 ** 6)
+    let paraSquare = parralax ** 2
+    let fluxPara = flux / paraSquare
+    let lumonisty = C * Math.PI * fluxPara
+    let distance = 1 / parralax
+    return `${starName} has a luminosity of ${lumonisty.toFixed(2)}L(sun).
+        It's distance from earth is ${distance.toFixed(2)}parsec
+        That is ${(distance * 3.26).toFixed(2)} light years \n`
+
+
+}
+//console.log(LumonistyCalculator("Procyon", '1.84 *(10**-8)', 0.286))
+console.log(LumonistyCalculator("SIRIUS", '1.00 *(10**-7)', 0.379))
+console.log(LumonistyCalculator("ARCTURUS", '2.79 *(10**-8)', 0.089))
+console.log(LumonistyCalculator("VEGA", '2.59 *(10**-8)', 0.129))
+console.log(LumonistyCalculator("BETELGEUSE", '1.75 *(10**-8)', 0.0076))
+console.log(LumonistyCalculator("ALTAIR", '1.32 *(10**-8)', 0.194))
+console.log(LumonistyCalculator("FORMALHAUT", '9.07 *(10**-9)', 0.130))
